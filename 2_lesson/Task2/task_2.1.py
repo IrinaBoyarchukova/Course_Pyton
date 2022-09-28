236ef5569709ae7603769bf1ea4b6d47d63c6f1b
Ирина <bojarchukova@mail.ru>
2022-09-28 16:22:59 +0800

    1. Напишите программу, которая принимает на вход число N и выдаёт последовательность из N членов.

--- 2_lesson/Task2/task_2.1.py
+++ 2_lesson/Task2/task_2.1.py
@@ -1,12 +1,12 @@
 # 1. Напишите программу, которая принимает на вход число N и выдаёт последовательность из N членов.
 # -------------------------------------- 1 вариант
 
-num = int(input())
-result = 1
+# num = int(input())
+# result = 1
 
-for i in range(num):
-    print(result, end=", ")
-    result *= -3
+# for i in range(num):
+#     print(result, end=", ")
+#     result *= -3
 
 
 # -------------------------------------- 2 вариант
@@ -15,3 +15,14 @@ for i in range(num):
 
 # for i in range(num):
 #     print((-3) ** i, end=", ")
+
+# ================================== 3 вариант
+
+n = int(input("Введите чиcло N: "))
+j = 1
+list1 = []
+for i in range(1, n + 1):
+    list1.append(str(j))
+    j *= -3
+print(", ".join(list1))
+
