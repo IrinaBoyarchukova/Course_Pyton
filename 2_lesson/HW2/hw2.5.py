4fb9a666161ca2df0ebb972efeb51d0e253ed96d
Ирина <bojarchukova@mail.ru>
2022-10-01 14:06:08 +0800

    # 5. **Реализуйте алгоритм перемешивания списка. #    Без функции shuffle из модуля random.

---
+++ 2_lesson/HW2/hw2.5.py
@@ -0,0 +1,20 @@
+# 5. **Реализуйте алгоритм перемешивания списка.
+#    Без функции shuffle из модуля random.
+
+from random import randrange
+
+num = int(input())
+nums_list = list(range(num))
+len_list = len(nums_list)
+
+print(nums_list)
+
+for i in range(len_list):
+    n_1 = randrange(len_list)
+    n_2 = randrange(len_list)
+    nums_list[n_1], nums_list[n_2] = nums_list[n_2], nums_list[n_1]
+
+print(nums_list)
+# 10
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [4, 0, 2, 3, 9, 6, 5, 7, 1, 8]
