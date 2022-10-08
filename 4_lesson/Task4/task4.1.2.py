52c17aa0878b62068c0f827d2b6e026e7dfeed2b
Ирина <bojarchukova@mail.ru>
2022-10-08 13:18:37 +0800

    # 1. Задайте строку из набора чисел. Напишите программу, #    которая покажет большее и меньшее число. #    В качестве символа-разделителя используйте пробел. # in 2 19 45 27 # out 2 45

--- 4_lesson/Task4/task4.1.2.py
+++ 4_lesson/Task4/task4.1.2.py
@@ -3,7 +3,15 @@
 #    В качестве символа-разделителя используйте пробел.
 # in 2 19 45 27
 # out 2 45
+# line = input()
+# list_1 = [x.strip(',.*') for x in line.split() if x.replace("-", "").isdigit()]
+# print(f'Min: {min(list_1)}')
+# print(f'Max: {max(list_1)}')
+
+#__________2______________
+
 line = input()
+# list_1 = [int(x.strip(',.*')) for x in line.split() if x.replace("-", "").isdigit()]
 list_1 = [x.strip(',.*') for x in line.split() if x.replace("-", "").isdigit()]
-print(f'Min: {min(list_1)}')
-print(f'Max: {max(list_1)}')
+print(f'Min: {min(list_1, key=int)}')
+print(f'Max: {max(list_1, key=int)}')
