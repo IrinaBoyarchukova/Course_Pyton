25ac0a4df3e8da7f8b90113f59e6f7b304abd201
Ирина <bojarchukova@mail.ru>
2022-10-08 14:28:33 +0800

    # 3. Задайте два числа. Напишите программу, которая найдёт #    НОК (наименьшее общее кратное) этих двух чисел.

--- 4_lesson/Task4/task4.3.py
+++ 4_lesson/Task4/task4.3.py
@@ -2,12 +2,15 @@
 #    НОК (наименьшее общее кратное) этих двух чисел.
 # https://zaochnik.com/spravochnik/matematika/delimost/naimenshee-obschee-kratnoe-nok/
 
+# from math import gcd
+# def nok(a, b):
+#     return (a * b) // gcd(a, b)
+# # 14 18 -> 126, 17 11 -> 187
+# print(nok(int(input()), int(input())))
+
+#___________2________
 from math import gcd
-
-
-def nok(a, b):
-    return (a * b) // gcd(a, b)
-
-
+a=int(input("a= "))
+b=int(input("b= "))
 # 14 18 -> 126, 17 11 -> 187
-print(nok(int(input()), int(input())))
+print(a * b // gcd(a, b))
